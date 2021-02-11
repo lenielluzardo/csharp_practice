d012b3332483a5d3940a1c0c47f12148f26dc766
Leniel Luzardo <contact@lenielluzardo.com>
2021-02-11 16:59:00 -0300

    [Algorithms & Data Structures] [Implementation] Singly & Doubly Linked List

---
+++ Output/Algorithms-DataStructure/SinglyLinkedList.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Output.Algorithms_DataStructure
+{
+  public class SinglyLinkedList
+  {
+    public SinglyLinkedList(int value)
+    {
+      Value = value;
+    }
+    public int Value { get; set; }
+    public SinglyLinkedList Next { get; set; }
+
+    public static void Exe()
+    {
+      var head = new SinglyLinkedList(1);
+      head.Next = new SinglyLinkedList(2);
+      head.Next.Next = new SinglyLinkedList(3);
+
+      IterateList(head);
+    }
+
+    private static void IterateList(SinglyLinkedList list)
+    {
+         if (list == null)
+         {
+            Console.WriteLine("No more nodes in the list");
+            return;
+         }
+
+         Console.WriteLine("Node at Position:.... {0}", list.Value);
+      
+         IterateList(list.Next);
+    }
+
+  }
+}
--- Output/Program.cs
+++ Output/Program.cs
@@ -1,16 +1,44 @@
-﻿using System;
+﻿using Output.Algorithms_DataStructure;
+using System;
+using System.Collections.Generic;
 
 namespace Output
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            //PalindromeExe.RunExercise();
-            //FizzBuzzExe.RunExercise();
-            PermutationExe.RunExercise();
-           
-            Console.ReadLine();
-        }
-    }
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         //PalindromeExe.RunExercise();
+         //FizzBuzzExe.RunExercise();
+         //SinglyLinkedList.Exe();
+         DoublyLinkedList.Exe();
+
+
+
+         Console.Read();
+      }
+   }
+   public class X
+   {
+      public static List<string> staticMethod(int n, List<string> result = null)
+      {
+         result = result != null ? result : new List<string>();
+         if (n == 0)
+         {
+            result.Add("No more bottles");
+            return result;
+         }
+         string str = n + "Bottles";
+         result.Add(str);
+         return staticMethod(n - 1, result);
+      }
+   }
+   public class Y : X
+   {
+      public static void staticMethod()
+      {
+         Console.WriteLine("Class Y");
+
+      }
+   }
 }
