a2a6be6795e5bbe6a7376ce092424f8ed0ee8e3c
Leniel Luzardo <contact@lenielluzardo.com>
2021-02-24 21:09:11 -0300

    [Implementation] FileSystemWatcher | ConcurrentDictionary | CacheMemory File Processing

--- Output/Files_Streams/FileProcessor.cs
+++ Output/Files_Streams/FileProcessor.cs
@@ -3,17 +3,23 @@ using System.Collections.Generic;
 using System.Text;
 using static System.Console;
 using System.IO;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Runtime.Caching;
 
 namespace Output.Files_Streams
 {
    /// <summary>
-   /// Custom input files processor. 
+   /// Process files through a console application.
    /// </summary>
    public class FileProcessor
    {
       private static readonly string BackupDirectoryName = "backup";
       private static readonly string InProgressDirectoryName = "processing";
       private static readonly string CompletedDirectoryName = "completed";
+
+      private static ConcurrentDictionary<string, string> FilesToProcessDictionary = new ConcurrentDictionary<string, string>();
+      private static MemoryCache FilesToProcessCache = MemoryCache.Default;
       public FileProcessor(string filePath)
       {
          InputFilePath = filePath;
@@ -87,8 +93,8 @@ namespace Output.Files_Streams
 
          File.Move(inProgressFilePath, completedFilePath);
 
-         string inProgressDirectoryPath = Path.GetDirectoryName(inProgressFilePath);
-         Directory.Delete(inProgressDirectoryPath, true);
+         //string inProgressDirectoryPath = Path.GetDirectoryName(inProgressFilePath); // When using cache the process of files is executed in parallel and throws an exception is inProgress folder isn't found.
+         //Directory.Delete(inProgressDirectoryPath, true);
       }
 
       private void ProcessTextFile(string inProgressFilePath)
@@ -104,8 +110,7 @@ namespace Output.Files_Streams
       /// <param name="filePath"></param>
       public static void ProcessSingleFile(string filePath)
       {
-         var fileProcessor = new
-            FileProcessor(filePath);
+         var fileProcessor = new FileProcessor(filePath);
          fileProcessor.Process();
       }
       public static void ProcessDirectory(string directoryPath, string fileType)
@@ -128,5 +133,168 @@ namespace Output.Files_Streams
          }
 
       }
+      #region Console Arguments Validations
+      /// <summary>
+      /// Checks if the arguments passed to the console are supported.
+      /// </summary>
+      /// <param name="args">Console 'flags'</param>
+      public static void ValidateConsoleArgs(string[] args)
+      {
+         var directoryToWatch = args[0];
+         if (!Directory.Exists(directoryToWatch))
+         {
+            WriteLine($"ERROR: {directoryToWatch} does not exist.");
+         }
+         else
+         {
+            WriteLine($"Watching directory {directoryToWatch} for changes.");
+
+            ProcessExistingFiles(directoryToWatch);
+
+            using (var inputFileWatcher = new FileSystemWatcher(directoryToWatch))
+            //using (var timer = new Timer(ProcessFilesWithConcurrentDictionary, null, 0, 1000))  // Use this for FilesToProcess ConcurrentDictionary
+            {
+               inputFileWatcher.IncludeSubdirectories = false;
+               inputFileWatcher.InternalBufferSize = 32768; // 32KB
+               inputFileWatcher.Filter = "*.*";
+               inputFileWatcher.NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.FileName;
+
+               // Register Event Handlers
+               inputFileWatcher.Created += FileCreated;
+               inputFileWatcher.Changed += FileChanged;
+               inputFileWatcher.Deleted += FileDeleted;
+               inputFileWatcher.Renamed += FileRenamed;
+               inputFileWatcher.Error += WatcherError;
+
+               inputFileWatcher.EnableRaisingEvents = true;
+
+               WriteLine("Press enter to quit.");
+               ReadLine();
+
+            }
+         }
+
+
+      }
+
+      private static void ProcessExistingFiles(string inputDirectory)
+      {
+         WriteLine($"Checking {inputDirectory} for existing files");
+
+         foreach(var filePath in Directory.EnumerateFiles(inputDirectory))
+         {
+            WriteLine($"  - Found {filePath}");
+            AddToCache(filePath);
+         }
+         
+      }
+
+
+
+      /// <summary>
+      /// Checks if the arguments passed to the console are supported.
+      /// </summary>
+      /// <param name="args">Console 'flags'</param>
+      public static void ValidateIndividualConsoleArgs(string[] args)
+      {
+         var command = args[0];
+
+
+         if (command == "--file")
+         {
+            var filePath = args[1];
+            WriteLine($"Single file {filePath} selected");
+            FileProcessor.ProcessSingleFile(filePath);
+         }
+         else if (command == "--dir")
+         {
+            var directoryPath = args[1];
+            var fileType = args[2];
+            WriteLine($"Directory {directoryPath} selected for {fileType} files");
+            FileProcessor.ProcessDirectory(directoryPath, fileType);
+         }
+         else
+         {
+            WriteLine("Invalid command line options");
+         }
+
+         WriteLine("Press enter to quit.");
+         ReadLine();
+      }
+      #endregion
+
+      #region Event Handlers
+      private static void WatcherError(object sender, ErrorEventArgs e)
+      {
+         WriteLine($"ERROR: file system watching may no longer be active: {e.GetException()}");
+      }
+
+      private static void FileRenamed(object sender, RenamedEventArgs e)
+      {
+         WriteLine($"* File renamed: {e.Name} - type: {e.ChangeType}");
+      }
+
+      private static void FileDeleted(object sender, FileSystemEventArgs e)
+      {
+         WriteLine($"* File deleted: {e.Name} - type: {e.ChangeType}");
+      }
+
+      private static void FileChanged(object sender, FileSystemEventArgs e)
+      {
+         WriteLine($"* File changed: {e.Name} - type: {e.ChangeType}");
+         AddToCache(e.FullPath);
+         //FilesToProcessDictionary.TryAdd(e.FullPath, e.FullPath); // // Use this for FilesToProcess ConcurrentDictionary
+      }
+      private static void FileCreated(object sender, FileSystemEventArgs e)
+      {
+         WriteLine($"* File created: {e.Name} - type: {e.ChangeType}");
+
+         //var fileProcessor = new FileProcessor(e.FullPath);
+         //fileProcessor.Process();
+         
+         AddToCache(e.FullPath);
+
+         //FilesToProcessDictionary.TryAdd(e.FullPath, e.FullPath); // Use this for FilesToProcess ConcurrentDictionary
+      }
+      #endregion
+      private static void AddToCache(string fullPath)
+      {
+         var item = new CacheItem(fullPath, fullPath);
+
+         var policy = new CacheItemPolicy
+         {
+            RemovedCallback = ProcessFilesWithCache,
+            SlidingExpiration = TimeSpan.FromSeconds(2),
+         };
+
+         FilesToProcessCache.Add(item, policy);
+
+      }
+      private static void ProcessFilesWithCache(CacheEntryRemovedArguments args)
+      {
+         WriteLine($"* Cache item removed: {args.CacheItem.Key} because {args.RemovedReason}");
+
+         if(args.RemovedReason == CacheEntryRemovedReason.Expired)
+         {
+            var fileProcessor = new FileProcessor(args.CacheItem.Key);
+            fileProcessor.Process();
+         }
+         else
+         {
+            WriteLine($"WARNING: {args.CacheItem.Key} was removed unexpectedly and may not be processed");
+         }
+      }
+      private static void ProcessFilesWithConcurrentDictionary(Object stateInfo)
+      {
+         foreach(var fileName in FilesToProcessDictionary.Keys)
+         {
+            if(FilesToProcessDictionary.TryRemove(fileName, out _))
+            {
+               var fileProcessor = new FileProcessor(fileName);
+               fileProcessor.Process();
+            }
+         }
+      }
+
    }
 }
--- Output/Program.cs
+++ Output/Program.cs
@@ -19,39 +19,13 @@ namespace Output
          //SinglyLinkedList.Exe();
          //DoublyLinkedList<int>.Exe();
 
-         ValidateConsoleArgs(args);
-
-         Console.Read();
+         FileProcessor.ValidateConsoleArgs(args);
       }
-      /// <summary>
-      /// Checks if the arguments passed to the console are supported.
-      /// </summary>
-      /// <param name="args">Console 'flags'</param>
-      static void ValidateConsoleArgs(string[] args)
-      {
-         var command = args[0];
 
-         if(command == "--file")
-         {
-            var filePath = args[1];
-            WriteLine($"Single file {filePath} selected");
-            FileProcessor.ProcessSingleFile(filePath);
-         }
-         else if (command == "--dir")
-         {
-            var directoryPath = args[1];
-            var fileType = args[2];
-            WriteLine($"Directory {directoryPath} selected for {fileType} files");
-            FileProcessor.ProcessDirectory(directoryPath, fileType);
-         }
-         else
-         {
-            WriteLine("Invalid command line options");
-         }
+      
+     
 
-         WriteLine("Press enter to quit.");
-         ReadLine();
-      }
+      
       
    }
 }
